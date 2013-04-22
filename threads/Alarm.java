856f3d860107bf244eaa9750b15e4bf9e2cac685
YICHI ZHANG <zhych125@gmail.com>
2013-04-22 00:14:39 -0700

    nachos proj1 task1,2,3 completed

---
+++ threads/Alarm.java
@@ -0,0 +1,81 @@
+package nachos.threads;
+
+import java.util.LinkedList;
+
+import nachos.machine.*;
+
+/**
+ * Uses the hardware timer to provide preemption, and to allow threads to sleep
+ * until a certain time.
+ */
+public class Alarm {
+    /**
+     * Allocate a new Alarm. Set the machine's timer interrupt handler to this
+     * alarm's callback.
+     *
+     * <p><b>Note</b>: Nachos will not function correctly with more than one
+     * alarm.
+     */
+    public Alarm() {
+	Machine.timer().setInterruptHandler(new Runnable() {
+		public void run() { timerInterrupt(); }
+	    });
+	wakeTimeList = new LinkedList<Long>();
+    }
+
+    /**
+     * The timer interrupt handler. This is called by the machine's timer
+     * periodically (approximately every 500 clock ticks). Causes the current
+     * thread to yield, forcing a context switch if there is another thread
+     * that should be run.
+     */
+    public void timerInterrupt() {
+	
+	boolean initStatus = Machine.interrupt().disable();
+	
+	while (!wakeTimeList.isEmpty())
+	{
+		if (wakeTimeList.getFirst()<Machine.timer().getTime())
+		{
+			wakeTimeList.removeFirst();
+			waitQueue.nextThread().ready();
+		}
+		else break;
+	}
+	
+	Machine.interrupt().restore(initStatus);
+	
+	KThread.currentThread().yield();
+	}
+
+    /**
+     * Put the current thread to sleep for at least <i>x</i> ticks,
+     * waking it up in the timer interrupt handler. The thread must be
+     * woken up (placed in the scheduler ready set) during the first timer
+     * interrupt where
+     *
+     * <p><blockquote>
+     * (current time) >= (WaitUntil called time)+(x)
+     * </blockquote>
+     *
+     * @param	x	the minimum number of clock ticks to wait.
+     *
+     * @see	nachos.machine.Timer#getTime()
+     */
+    public void waitUntil(long x) {
+	// for now, cheat just to get something working (busy waiting is bad)
+    boolean initStatus = Machine.interrupt().disable();
+    
+    long wakeTime = Machine.timer().getTime() + x;
+    
+    wakeTimeList.add(wakeTime);
+	
+    waitQueue.waitForAccess(KThread.currentThread());
+    
+    KThread.sleep();
+    
+    Machine.interrupt().restore(initStatus);
+    }
+    private static ThreadQueue waitQueue = ThreadedKernel.scheduler.newThreadQueue(false);
+    private static LinkedList<Long> wakeTimeList;
+}
---
+++ threads/Condition2.java
@@ -0,0 +1,89 @@
+package nachos.threads;
+
+import java.util.LinkedList;
+
+import nachos.machine.*;
+
+/**
+ * An implementation of condition variables that disables interrupt()s for
+ * synchronization.
+ *
+ * <p>
+ * You must implement this.
+ *
+ * @see	nachos.threads.Condition
+ */
+public class Condition2 {
+    /**
+     * Allocate a new condition variable.
+     *
+     * @param	conditionLock	the lock associated with this condition
+     *				variable. The current thread must hold this
+     *				lock whenever it uses <tt>sleep()</tt>,
+     *				<tt>wake()</tt>, or <tt>wakeAll()</tt>.
+     */
+    public Condition2(Lock conditionLock) {
+	this.conditionLock = conditionLock;
+	waitQueue = new LinkedList<KThread>();
+    }
+
+    /**
+     * Atomically release the associated lock and go to sleep on this condition
+     * variable until another thread wakes it using <tt>wake()</tt>. The
+     * current thread must hold the associated lock. The thread will
+     * automatically reacquire the lock before <tt>sleep()</tt> returns.
+     */
+    public void sleep() {
+	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+	
+	boolean initStatus = Machine.interrupt().disable();
+	
+	conditionLock.release();
+
+	waitQueue.add(KThread.currentThread());
+	
+	KThread.sleep();
+
+	conditionLock.acquire();
+    
+	Machine.interrupt().restore(initStatus);
+    }
+
+    /**
+     * Wake up at most one thread sleeping on this condition variable. The
+     * current thread must hold the associated lock.
+     */
+    public void wake() {
+	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+	
+	boolean initStatus = Machine.interrupt().disable();
+	
+	if (!waitQueue.isEmpty())
+		
+		waitQueue.removeFirst().ready();
+	
+	Machine.interrupt().restore(initStatus);
+	
+    }
+
+    /**
+     * Wake up all threads sleeping on this condition variable. The current
+     * thread must hold the associated lock.
+     */
+    public void wakeAll() {
+	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+	
+	boolean initStatus = Machine.interrupt().disable();
+	
+	if  (!waitQueue.isEmpty())
+	{
+		for (KThread thread = waitQueue.removeFirst(); !waitQueue.isEmpty(); thread = waitQueue.removeFirst())
+		
+			thread.ready();
+	}
+		Machine.interrupt().restore(initStatus);
+    }
+
+    private Lock conditionLock;
+    private LinkedList<KThread> waitQueue;
+}
